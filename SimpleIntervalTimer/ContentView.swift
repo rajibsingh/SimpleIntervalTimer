31fdcc30d6152e2615b08df101bab4062289f0aa
rajibsingh <rajibsingh@yahoo.com>
2023-11-27 17:07:54 -0500

    Initial Commit

---
+++ SimpleIntervalTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleIntervalTimer
+//
+//  Created by Rajib Singh on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleIntervalTimer/SimpleIntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleIntervalTimerApp.swift
+//  SimpleIntervalTimer
+//
+//  Created by Rajib Singh on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleIntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
