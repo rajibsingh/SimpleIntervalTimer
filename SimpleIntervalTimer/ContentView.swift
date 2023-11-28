42c661deafcfc7f70c4c636eba5a03886d037321
rajibsingh <rajibsingh@yahoo.com>
2023-11-27 22:30:29 -0500

    I have a text field and a button now lol

--- SimpleIntervalTimer/ContentView.swift
+++ SimpleIntervalTimer/ContentView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textFieldValue = "unset"
+    
     var body: some View {
         
         var timer = Person(name: "frank", age: 10)
         
         VStack {
-            Button("click me") {
-                print("clicked!")
-                timer.greet()
+            
+            HStack {
+                Text(textFieldValue)
+                    .font(.title)
+                Button("click me") {
+                    print("clicked!")
+                    timer.greet()
+                    textFieldValue = "hoo boy"
+                }
+                .foregroundColor(.green)
             }
-            .foregroundColor(.green)
         }
         .padding()
     }
